f54338772c05b8428705072cfcfec31e182e3e91
Fengxiang Zhu <wow2009zfx@gmail.com>
2016-04-12 12:06:01 +0800

    Update httpProxy.sh

--- httpProxy.sh
+++ httpProxy.sh
@@ -9,7 +9,7 @@ then
 else 
     networksetup -setwebproxy $network $httpproxy $port off
     networksetup -setsecurewebproxy $network $httpproxy $port off
-    networksetup -setproxybypassdomains $network 127.0.0.1 192.168.0.0/16 10.0.0.0/8 172.16.0.0/12 localhost *.local jenkins.nanchao.org
+    networksetup -setproxybypassdomains $network 127.0.0.1 192.168.0.0/16 10.0.0.0/8 172.16.0.0/12 localhost *.local
     networksetup -setautoproxystate $network off
     networksetup -setsocksfirewallproxystate $network off
     echo successfully setup proxy
